0ca0fb465bae1f29e041917832c211243b0bf90c
Henrik Kirk <henrik@c3a.dk>
2014-08-28 13:37:12 +0200

    Added just-i18n stubs

---
+++ package-stubber/community-stubs/just-i18n.js
@@ -0,0 +1,14 @@
+/**
+ * Created by henrik on 27/08/14.
+ */
+
+var emptyFn = function() {};
+var emptyStringFn = function () {return "";};
+
+i18n = { //function () {
+  setLanguage: emptyFn,
+  setDefaultLanguage: emptyFn,
+  getLanguage: emptyStringFn,
+  showMissing: emptyFn,
+  map: emptyFn
+};
